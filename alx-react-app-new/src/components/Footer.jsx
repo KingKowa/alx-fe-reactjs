acf7cb4e04d1fd729fec09066b851f17ba2256f2
Frederick Ankomah <ankomahfrederickyahoo@gmail.com>
2024-11-08 00:09:30 +0000

    Apply inline css styling to react components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "skyblue",
+        color: "white",
+        textAlign: "center",
+      }}
+    >
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
