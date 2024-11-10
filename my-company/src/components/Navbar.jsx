c29720a66b1dc5fbcf38bd9b12beda4a8939c8e9
Frederick Ankomah <ankomahfrederickyahoo@gmail.com>
2024-11-10 22:31:12 +0000

    Apply inline CSS styles

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,15 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
+    <nav
+      style={{
+        padding: "10px",
+        borderBottom: "1px solid #ccc",
+        backgroundColor: "navy",
+        color: "white",
+        textAlign: "center",
+      }}
+    >
       <Link to="/" style={{ margin: "0 10px" }}>
         Home
       </Link>
