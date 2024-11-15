2dacd8ae9e3504a9a90e2370d9089602e8ad91b1
Frederick Ankomah <ankomahfrederickyahoo@gmail.com>
2024-11-15 15:24:53 +0000

    Implementing Advanced Search and Filtering Capabilities

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -7,6 +7,17 @@ const useRecipeStore = create((set) => ({
 
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+  updateRecipe: (id, updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+      ),
+    })),
+  setRecipes: (recipes) => set({ recipes }),
 
   // Set the search term and trigger filtering
   setSearchTerm: (term) =>
