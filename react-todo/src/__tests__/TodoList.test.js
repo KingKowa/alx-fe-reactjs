8c9069c3d1330c2b94577a3c0712b5983f2ea85f
Frederick Ankomah <ankomahfrederickyahoo@gmail.com>
2024-11-24 20:35:53 +0000

    Implement and Test a Todo List Component in React

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import TodoList from "./TodoList";
+import TodoList from "../components/TodoList";
 
 const sampleTodos = [
   { id: 1, text: "Test Todo 1", completed: false },
