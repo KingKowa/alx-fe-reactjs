abcf00e860dde1934f512fe481881b43ea5e3ccd
Frederick Ankomah <ankomahfrederickyahoo@gmail.com>
2024-11-28 08:55:20 +0000

    Setting Up Tailwind CSS in a New React Project

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], //added
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: { extend: {} }, //added
+  plugins: [],
+};
